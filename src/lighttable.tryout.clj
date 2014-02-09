fd225dcba3d44c3706feb3de7e6a9c27ad57472c
mbonaci <mbonaci@gmail.com>
2014-02-09 23:27:45 +0100

    add some examples to src

--- src/lighttable.tryout.clj
+++ src/lighttable.tryout.clj
@@ -2,4 +2,12 @@
 ;; immediately with the results shown on the
 ;; right.
 
-(+ 2 3)
+(take 9 (cycle [1 2 3 4]))
+
+(reverse "woolf")
+(apply str (reverse "woolf"))
+(str (reverse "woolf"))
+
+(doto (new javax.swing.JFrame) (.add (javax.swing.JLabel. "Hello")) .pack .show)
+
+(type javax.swing.JFrame)
