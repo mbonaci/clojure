6af2f6727c16141ab3da2d2936ce764f4b98b384
mbonaci <mbonaci@gmail.com>
2014-02-14 01:28:14 +0100

    Add mbo-unbalanced-binary-tree example

---
+++ src/mbo-unbalanced-binary-tree.clj
@@ -0,0 +1,28 @@
+(ns mbo-unbalanced-binary-tree)
+
+(defn xseq [t]
+  (when t
+    (concat (xseq (:L t)) [(:val t)] (xseq (:R t)))))
+
+(defn xconj [t v]
+  (cond
+    (nil? t) {:val v, :L nil, :R nil}
+    (< v (:val t))
+      {:val (:val t),
+       :L (xconj (:L t) v),
+       :R (:R t)}
+     (> v (:val t))
+      {:val (:val t)
+       :L (:L t)
+       :R (xconj (:R t) v)}
+   ))
+
+(def f1 (xconj nil 50))
+(def f2 (xconj f1 40))
+(def f3 (xconj f2 60))
+(def f4 (xconj f3 10))
+(def f5 (xconj f4 30))
+(def f6 (xconj f5 20))
+(def f7 (xconj f6 35))
+
+(xseq f7)
